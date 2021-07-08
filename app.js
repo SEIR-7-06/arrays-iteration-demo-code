56e569e5a3c47b7493f1a60b9f80da0240dca866
msolorio <msolorio.github@gmail.com>
2021-07-08 15:24:14 -0700

    Adds demo code for if/else, and for loops

---
+++ app.js
@@ -0,0 +1,90 @@
+// DATA TYPES
+
+// Strings
+// Booleans
+// Numbers
+// Arrays
+// Undefined
+// Null
+
+// Assignment Operator (=)
+// const personName = 'Phil';
+
+// const message = 'My name is ' + personName;
+
+// console.log(message);
+
+//////////////////////////////////////
+const isSunny = false;
+
+// NOT Operator
+// console.log(!isSunny);
+
+// if (!isSunny) { // ???
+//   console.log('Stay inside')
+// }
+
+///////////////////////////////////////
+// Strict Equality (===)
+
+// const currentDay = 'Monday';
+
+// if (currentDay === 'Friday') { // ???
+//   console.log('Go out to a restaurant');
+// } else if (currentDay === 'Tuesday') { // ???
+//   console.log('Lets get takeout');
+// } else if (currentDay === 'Monday') {
+//   console.log('Take out the trash');
+// } else {
+//   console.log('Stay at home and make dinner');
+// }
+
+////////////////////////////////////////
+// For Loop
+
+// 0, 1, 2
+
+// For each one of these numbers
+// Say hello
+
+'hello'
+'hello'
+'hello'
+'hello'
+
+for (let number = 0; number < 10; number++) {
+  console.log(number);
+}
+
+/////////////////////////////////////////////
+
+// for (let number = 0; number < 10; number++) {
+//   // If number is even
+//   if (number % 3 === 0) {
+//     // then log out the number
+//     console.log(number);
+//   }
+// }
+
+/////////////////////////////////////////////
+// Create a for loop logging out the number
+// 5 to 10
+
+// for (let number = 5; number <= 10; number++) {
+//   console.log(number);
+// }
+
+// Create a for loop logging out the number
+// 10 to 0
+
+// for (let number = 10; number >= 0; number--) {
+//   console.log(number);
+// }
+
+
+// Create a for loop logging out from
+// 200 to 300 logging out every 3rd number
+
+// for (let number = 200; number <= 300; number += 3) {
+//   console.log(number);
+// }
