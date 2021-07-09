806df660beab887d196d916ee000393b9cc33726
msolorio <msolorio.github@gmail.com>
2021-07-08 17:19:50 -0700

    Adds code for loops and arrays

--- app.js
+++ app.js
@@ -52,15 +52,15 @@ const isSunny = false;
 'hello'
 'hello'
 
-for (let number = 0; number < 10; number++) {
-  console.log(number);
-}
+// for (let number = 0; number < 10; number++) {
+//   console.log(number);
+// }
 
 /////////////////////////////////////////////
 
 // for (let number = 0; number < 10; number++) {
 //   // If number is even
-//   if (number % 3 === 0) {
+//   if (number % 2 === 0) {
 //     // then log out the number
 //     console.log(number);
 //   }
@@ -88,3 +88,71 @@ for (let number = 0; number < 10; number++) {
 // for (let number = 200; number <= 300; number += 3) {
 //   console.log(number);
 // }
+
+////////////////////////////////////////////
+// ARRAYS
+// Store a list of related information
+
+const students = [
+  'Jill', // 0 
+  'Harold', // 1
+  'Dorothy', // 2
+  'Victor', // 3
+  'Heather'
+];
+
+// Create an array of your own and practice adding,
+// removing, and updating items in the array.
+
+// Pets
+// Hobbies
+// Favorite Restaurants
+// Favorite Cities
+
+console.log(students);
+
+// Log out a single student
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[3]);
+// console.log(students[4]);
+
+// Log the length of the array
+// console.log(students.length);
+
+// Updating an item in the array
+// students[1] = 'Harry';
+// console.log(students);
+
+// Add a item to the Array
+// students.push('Heather', 'Kyle');
+// students.push('Jerry');
+// console.log(students);
+
+// students.splice(2, 0, 'Phil');
+// console.log(students);
+
+// Remove an item from the Array
+// students.pop();
+// console.log(students);
+
+// students.splice(1, 2);
+// console.log(students);
+
+//////////////////////////////////////////
+// Combining Arrays with Loops
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+
+// For each student
+// Log out that student
+
+for (let index = 0; index < students.length; index++) {
+  console.log(students[index]);
+}
+
+//////////////////////////////////////////////
+// Using your array that you created
+// Use a for loop to print out each item
